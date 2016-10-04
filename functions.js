8d61d9cae2a9ed259ef4fb833357fb28c8016ee8
FalconDesign <hebgax0@gmail.com>
2016-10-05 00:26:02 +0300

    Check partners

---
+++ functions.js
@@ -0,0 +1,9 @@
+"use strict"
+
+$(function () {
+    var spinner = $('.spinner');
+        spinner.fadeOut(function () {
+            $('.heading, .faq, .numbers, .write, .email').addClass('visible');
+        });
+
+  });
